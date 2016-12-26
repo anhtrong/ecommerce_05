b2e426783911173976c01ab203b6ee48f860c29a
anhtrong <anhtrong993@gmail.com>
2016-12-26 09:35:02 +0700

    share-products

---
+++ ecommerce/app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+  load_and_authorize_resource
+  before_action :load_product, only: :create
+
+  def create
+    @comment = @product.comments.build comment_params
+    if @comment.save
+      flash[:success] = t "success_create"
+    else
+      flash[:danger] = t "error_create"
+    end
+    redirect_to @product
+  end
+
+  def destroy
+    if @comment.destroy
+      flash.now[:success] = t "success_delete"
+    else
+      flash.now[:danger] = t "error_delete"
+    end
+    respond_to do |format|
+      format.json do
+        render json: {flash: flash}
+      end
+    end
+  end
+
+  private
+  def load_product
+    @product = Product.find_by id: comment_params[:product_id]
+    unless @product
+      flash[:danger] = t "error"
+      redirect_to :back
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit :content, :user_id, :product_id
+  end
+end
