58804f48a776187de8c0f64db07154e3949f2023
anhtrong <anhtrong993@gmail.com>
2017-01-03 09:17:51 +0700

    cart-product

--- ecommerce/app/controllers/application_controller.rb
+++ ecommerce/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :initialize_cart
+
+  private
+  def initialize_cart
+    @cart = Cart.build_form_hash session
+  end
 end
